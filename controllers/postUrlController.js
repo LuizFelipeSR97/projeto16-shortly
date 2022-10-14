86fe5e541b083540acbaddd61164508d6c5385d4
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-14 15:58:19 -0300

    fixed url verification

--- controllers/postUrlController.js
+++ controllers/postUrlController.js
@@ -1,19 +1,19 @@
 import {connection} from '../db/db.js';
 import {nanoid} from 'nanoid';
 
-function isURL(link) {
-    var regexp = /^(ftp|http|https|chrome|:\/\/|\.|@){2,}(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\S*:\w*@)*([a-zA-Z]|(\d{1,3}|\.){7}){1,}(\w|\.{2,}|\.[a-zA-Z]{2,3}|\/|\?|&|:\d|@|=|\/|\(.*\)|#|-|%)*$/gum
-    return regexp.test(link);
-}
-
 async function postUrl(req,res){
+
+    function verifyURL(link) {
+        var regexp = /^(ftp|http|https|chrome|:\/\/|\.|@){2,}(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\S*:\w*@)*([a-zA-Z]|(\d{1,3}|\.){7}){1,}(\w|\.{2,}|\.[a-zA-Z]{2,3}|\/|\?|&|:\d|@|=|\/|\(.*\)|#|-|%)*$/gum
+        return regexp.test(link);
+    }
 
     const {url} = req.body;
 
-    const isURL = isURL(url)
+    const isURL = verifyURL(url)
 
     if (!isURL){
-        res.sendStatus(422)
+        return res.sendStatus(422)
     }
 
     const shortUrl = nanoid()
