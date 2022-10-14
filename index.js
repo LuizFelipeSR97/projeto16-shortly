6777847d2c7c56d1f3d5f120f0d5ce32b76d4185
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-13 21:59:06 -0300

    ranking get request created

--- index.js
+++ index.js
@@ -172,6 +172,16 @@ server.get('/urls/open/:shortUrl', async (req,res) => {
     return res.redirect(url)
 });
 
+server.get('/ranking', async (req,res) => {
+
+    const ranking = await connection.query(`SELECT urls."userId" AS id, users.name, COUNT (urls."userId") AS "linksCount", SUM(urls.visitors) AS "visitCount" FROM urls JOIN users ON urls."userId" = users.id GROUP BY urls."userId", users.name ORDER BY "visitCount" DESC LIMIT 10
+;`)
+
+
+
+    return res.status(200).send(ranking.rows)
+});
+
 
 
 // --------------------
