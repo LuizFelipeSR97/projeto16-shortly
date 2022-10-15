c724cce623fa232fb30b4f8d1d683576d3048dc1
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-15 11:58:13 -0300

    connection string added to dotenv and security problem fixed

--- db/db.js
+++ db/db.js
@@ -1,13 +1,9 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const {Pool} = pg;
 
-const connection = new Pool ({
-    user: 'postgres',
-    password: '1401',
-    host: 'localhost',
-    port: 5432,
-    database: 'shortly'
-})
+const connection = new Pool ({connectionString: process.env.DATABASE_URL,});
 
 export {connection}
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
 import signUp from './routes/signUpRoute.js';
 import signIn from './routes/signInRoute.js';
@@ -10,9 +9,9 @@ import redirect from './routes/redirectRoute.js';
 import deleteUrl from './routes/deleteUrlRoute.js';
 import userInfo from './routes/userInfoRoute.js';
 import ranking from './routes/rankingRoute.js';
+import { connection } from './db/db.js';
 
 const server = express();
-dotenv.config();
 
 server.use(express.json());
 server.use(cors());
@@ -25,20 +24,14 @@ server.use(deleteUrl);
 server.use(userInfo);
 server.use(ranking);
 
+server.get('/users', async (req,res) => {
+    res.send(await connection.query('SELECT * FROM users;'))
+});
+
 server.listen(process.env.PORT, ()=> console.log(`Server running on port ${process.env.PORT}`));
 
 
 /* Falta:
     - Fazer o dump
-    - Fazer a verificacao no body da route (/urls/shorten) se a url é valida com REGEX
-    - Colocar DATABASE_URL no dotenv (o que seria?)
     - Fazer o deploy do backend e do banco com o Heroku
- */
-
-    /*
-    REGEX
-    
-    function isURL(link) {
-        var regexp = /^(ftp|http|https|chrome|:\/\/|\.|@){2,}(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\S*:\w*@)*([a-zA-Z]|(\d{1,3}|\.){7}){1,}(\w|\.{2,}|\.[a-zA-Z]{2,3}|\/|\?|&|:\d|@|=|\/|\(.*\)|#|-|%)*$/gum
-        return regexp.test(link);
-    } */
+*/
