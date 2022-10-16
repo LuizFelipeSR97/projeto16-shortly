4c48d30b1a171450ca4f63fd5d74188e9e09277c
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-16 11:26:00 -0300

    pg problem again

--- controllers/userInfoController.js
+++ controllers/userInfoController.js
@@ -5,24 +5,15 @@ async function userInfo(req,res){
     let urlsSearched = await connection.query('SELECT id, "shortUrl", url, visitors AS "visitCount" FROM urls	WHERE "userId" = $1;',[res.locals.userId])
 
     if (urlsSearched.rowCount===0){
-        const userUrls=[];
-    } else {
-        const userUrls=urlsSearched.rows;
+        const simpleUserSearched = await connection.query('SELECT * FROM users WHERE id = $1',[res.locals.userId])
+        return res.send({id: simpleUserSearched.rows[0].id, name: simpleUserSearched.rows[0].name, visitCount: 0, shortenedUrls: []})
     }
-
+    
     const userSearched = await connection.query('SELECT urls."userId" AS id, users.name, SUM(urls.visitors) AS "visitCount" FROM urls JOIN users ON urls."userId" = users.id WHERE urls."userId" = $1 GROUP BY urls."userId", users.name;',[res.locals.userId])
 
-    return res.send(userSearched)
-
-    if (userSearched.rowCount===0){
-        const newUserSearched = await connection.query('SELECT * FROM users WHERE id = $1;',[res.locals.userId])
-        const response = {id: res.locals.userId, name: newUserSearched.rows[0].name, visitCount: 0, shortenedUrls: []}
-    } else {
-        const response = {id: userSearched.rows[0].id, name: userSearched.rows[0].name, visitCount: userSearched.rows[0].visitCount, shortenedUrls: userUrls}
-    }
+    const response = {id: userSearched.rows[0].id, name: userSearched.rows[0].name, visitCount: userSearched.rows[0].visitCount, shortenedUrls: userUrls}
 
     return res.send(response)
-
 }
 
-export {userInfo};
+export {userInfo}
