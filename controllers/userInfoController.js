af4a549260832433c6e2783217aa3e984c731b3f
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-16 00:49:55 -0300

    try to solve user/me problem

--- controllers/userInfoController.js
+++ controllers/userInfoController.js
@@ -2,28 +2,19 @@ import {connection} from '../db/db.js';
 
 async function userInfo(req,res){
 
-    console.log("user Id")
-    console.log(res.locals.userId)
-
     let urlsSearched = await connection.query('SELECT id, "shortUrl", url, visitors AS "visitCount" FROM urls	WHERE "userId" = $1;',[res.locals.userId])
 
-    const userUrls = urlsSearched.rows
-    console.log(userUrls)
+    //return res.send(urlsSearched)
 
     const userSearched = await connection.query(`SELECT urls."userId" AS id, users.name, SUM(urls.visitors) AS "visitCount" FROM urls JOIN users ON urls."userId" = users.id WHERE urls."userId" = $1 GROUP BY urls."userId", users.name;`,[res.locals.userId])
 
-    return res.send(userSearched)
+    if (userSearched.rowCount===0){
+        const newUserSearched = await connection.query(`SELECT * FROM users WHERE id = $1;`[{res.locals.userId}])
+        const response = {id: res.locals.userId, name: newUserSearched.rows[0].name, visitCount: 0, shortenedUrls: []}
+        res.send(response)
+    }
 
-    console.log("UserSearched = ")
-    console.log(userSearched)
-
-    //const response = {id: userSearched.rows[0].id, name: userSearched.rows[0].name, visitCount: userSearched.rows[0].visitCount, shortenedUrls: userUrls}
-    const response = {
-        "id": 10,
-          "name": "teste",
-          "visitCount": 0,
-          "shortenedUrls": []
-      }
+    const response = {id: userSearched.rows[0].id, name: userSearched.rows[0].name, visitCount: userSearched.rows[0].visitCount, shortenedUrls: userUrls}
 
     return res.send(response)
 
