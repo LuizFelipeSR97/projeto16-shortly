dd7079c2adf37938d35eb95e62d9e06a2ecaf814
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-15 22:57:50 -0300

    another try

--- middlewares/solveCorsProblem.js
+++ middlewares/solveCorsProblem.js
@@ -1,7 +1,7 @@
 function solveCorsProblem(req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://projeto16-shortly-front-lake.vercel.app/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
