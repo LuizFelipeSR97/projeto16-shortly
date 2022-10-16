402509d686737d08e02edf9402144208482a74e8
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-16 00:54:44 -0300

    try to solve user/me problem

--- controllers/userInfoController.js
+++ controllers/userInfoController.js
@@ -4,12 +4,10 @@ async function userInfo(req,res){
 
     let urlsSearched = await connection.query('SELECT id, "shortUrl", url, visitors AS "visitCount" FROM urls	WHERE "userId" = $1;',[res.locals.userId])
 
-    return res.send(urlsSearched)
-
     const userSearched = await connection.query(`SELECT urls."userId" AS id, users.name, SUM(urls.visitors) AS "visitCount" FROM urls JOIN users ON urls."userId" = users.id WHERE urls."userId" = $1 GROUP BY urls."userId", users.name;`,[res.locals.userId])
 
     if (userSearched.rowCount===0){
-        const newUserSearched = await connection.query(`SELECT * FROM users WHERE id = $1;`[{res.locals.userId}])
+        const newUserSearched = await connection.query(`SELECT * FROM users WHERE id = $1;`[res.locals.userId])
         const response = {id: res.locals.userId, name: newUserSearched.rows[0].name, visitCount: 0, shortenedUrls: []}
         res.send(response)
     }
