2d5092c85f3a8e675c7ca63cd43e2360b2107587
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-15 22:56:02 -0300

    another try

--- routes/signUpRoute.js
+++ routes/signUpRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { signUp } from '../controllers/usersControllers.js';
 import {signUpValidation} from '../middlewares/validations.js';
+import {solveCorsProblem} from '../middlewares/solveCorsProblem.js';
 
 const router = express.Router();
 
-router.post('/signup', signUpValidation, signUp)
+router.post('/signup', solveCorsProblem, signUpValidation, signUp)
 
 export default router;
