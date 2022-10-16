f314409989ae231b4a4633019418944f81fa8958
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-16 10:56:29 -0300

    pg problem again

--- controllers/userInfoController.js
+++ controllers/userInfoController.js
@@ -4,6 +4,8 @@ async function userInfo(req,res){
 
     let urlsSearched = await connection.query('SELECT id, "shortUrl", url, visitors AS "visitCount" FROM urls	WHERE "userId" = $1;',[res.locals.userId])
 
+    return res.send(urlsSearched)
+
     const userUrls = urlsSearched.rows
 
     const userSearched = await connection.query('SELECT urls."userId" AS id, users.name, SUM(urls.visitors) AS "visitCount" FROM urls JOIN users ON urls."userId" = users.id WHERE urls."userId" = $1 GROUP BY urls."userId", users.name;',[res.locals.userId])
