ad28355e1193f45bc3499db7fe988cb0e2ce5128
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-10-16 11:36:00 -0300

    pg problem again

--- controllers/userInfoController.js
+++ controllers/userInfoController.js
@@ -2,7 +2,7 @@ import {connection} from '../db/db.js';
 
 async function userInfo(req,res){
 
-    let urlsSearched = await connection.query('SELECT id, "shortUrl", url, visitors AS "visitCount" FROM urls	WHERE "userId" = $1;',[res.locals.userId])
+    const urlsSearched = await connection.query('SELECT id, "shortUrl", url, visitors AS "visitCount" FROM urls	WHERE "userId" = $1;',[res.locals.userId])
 
     if (urlsSearched.rowCount===0){
         const simpleUserSearched = await connection.query('SELECT * FROM users WHERE id = $1',[res.locals.userId])
@@ -11,7 +11,7 @@ async function userInfo(req,res){
     
     const userSearched = await connection.query('SELECT urls."userId" AS id, users.name, SUM(urls.visitors) AS "visitCount" FROM urls JOIN users ON urls."userId" = users.id WHERE urls."userId" = $1 GROUP BY urls."userId", users.name;',[res.locals.userId])
 
-    const response = {id: userSearched.rows[0].id, name: userSearched.rows[0].name, visitCount: userSearched.rows[0].visitCount, shortenedUrls: urlsSearched}
+    const response = {id: userSearched.rows[0].id, name: userSearched.rows[0].name, visitCount: userSearched.rows[0].visitCount, shortenedUrls: urlsSearched.rows}
 
     return res.send(response)
 }
